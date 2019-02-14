172ab504d2c5ea8fc613f1fd7a198daf7f886d6b
haskellneal <33379305+haskellneal@users.noreply.github.com>
2019-02-13 17:24:26 -0800

    Add files via upload

---
+++ ShinyAppCP4.R
@@ -0,0 +1,73 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+
+
+library(shiny)
+
+# Define UI for miles per gallon app ----
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Miles Per Gallon"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for variable to plot against mpg ----
+     
+      selectInput("variable", "Variable:",
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear")),
+      
+      # Input: Checkbox for whether outliers should be included ----
+      checkboxInput("outliers", "Show outliers", TRUE)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Formatted text for caption ----
+      h3(textOutput("caption")),
+      
+      # Output: Plot of the requested variable against mpg ----
+      plotOutput("mpgPlot")
+      
+    )
+  )
+)
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  mpgData <- mtcars
+  mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+  # Compute the formula text ----
+  # This is in a reactive expression since it is shared by the
+  # output$caption and output$mpgPlot functions
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption ----
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg ----
+  # and only exclude outliers if requested
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()),
+            data = mpgData,
+            outline = input$outliers,
+            col = "#75AADB", pch = 19)
+  })
+  
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
+
